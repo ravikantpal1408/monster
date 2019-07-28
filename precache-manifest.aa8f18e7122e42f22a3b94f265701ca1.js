b589fe59e4197e006807a8a29620b31309ccdc4d
ravikantpal1408 <ravikantpal123@gmail.com>
2019-07-28 16:48:09 +0530

    Updates

---
+++ precache-manifest.aa8f18e7122e42f22a3b94f265701ca1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96060ea777f9d0b1f13886544e8e88f9",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "0fd372b7fd2b36f3f83c",
+    "url": "/monster/static/css/2.b54df117.chunk.css"
+  },
+  {
+    "revision": "29ce7fcdd95a8dbdad14",
+    "url": "/monster/static/css/main.87d75a6b.chunk.css"
+  },
+  {
+    "revision": "0fd372b7fd2b36f3f83c",
+    "url": "/monster/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "29ce7fcdd95a8dbdad14",
+    "url": "/monster/static/js/main.8b576528.chunk.js"
+  },
+  {
+    "revision": "24520cef794f93741fed",
+    "url": "/monster/static/js/runtime~main.488aa6c8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/monster/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/monster/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/monster/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/monster/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/monster/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
